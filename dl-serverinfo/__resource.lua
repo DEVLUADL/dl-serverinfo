2dd08ff9b35e23fbdae966312fd488423cc81ac6
DevLua <79162417+DEVLUADL@users.noreply.github.com>
2021-04-15 17:40:36 -0400

    Update __resource.lua

--- dl-serverinfo/__resource.lua
+++ dl-serverinfo/__resource.lua
@@ -1,4 +1,7 @@
-  
+Author JEVA | Fixed/Reworked By 𝘿𝙇 ®#5895
+
+description 'This is a server info script reworked by 𝘿𝙇 ®#5895
+
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 client_script 'help-c.lua' 
